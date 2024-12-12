1837c8714cfcbda664eb9f8f1e34f18565dc49b9
devdong9897 <dev.dong9897@gmail.com>
2024-12-12 23:36:09 +0900

    feat: State로 사용자 입력 관리하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import "./App.css";
-import { useState } from "react";
-import Bulb from "./components/Bulb";
-import Counter from "./components/Counter";
+import Register from "./components/Register";
 
 function App() {
   return (
     <>
-      <div>
-        <Bulb />
-        <Counter></Counter>
-      </div>
+      <Register />
     </>
   );
 }
